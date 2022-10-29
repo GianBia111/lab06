f4bffeec1ab5b76ec487872feefe657e2a735b9e
giangian2 <gianluca.bianchi8@studio.unibo.it>
2022-10-29 11:52:36 +0200

    second es finished right

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,6 +1,7 @@
 package it.unibo.collections;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
@@ -11,6 +12,12 @@ import java.util.Map;
  *
  */
 public final class UseListsAndMaps {
+    private static final long AFRICA_POP = 1_110_635_000L;
+    private static final long AMERICAS_POP = 972_005_000L;
+    private static final long ANTARCTICA_POP = 0L;
+    private static final long ASIA_POP = 4_298_723_000L;
+    private static final long EUROPE_POP = 742_452_000L;
+    private static final long OCEANIA_POP= 38_304_000L;
 
     private UseListsAndMaps() {
     }
@@ -59,54 +66,21 @@ public final class UseListsAndMaps {
         time = System.nanoTime() - time;
 
         System.out.println("Time required in order to add 100000 elelements to a linked list: "+time);
-        /*
-         * 1) Create a new ArrayList<Integer>, and populate it with the numbers
-         * from 1000 (included) to 2000 (excluded).
-         */
-        /*
-         * 2) Create a new LinkedList<Integer> and, in a single line of code
-         * without using any looping construct (for, while), populate it with
-         * the same contents of the list of point 1.
-         */
-        /*
-         * 3) Using "set" and "get" and "size" methods, swap the first and last
-         * element of the first list. You can not use any "magic number".
-         * (Suggestion: use a temporary variable)
-         */
-        /*
-         * 4) Using a single for-each, print the contents of the arraylist.
-         */
-        /*
-         * 5) Measure the performance of inserting new elements in the head of
-         * the collection: measure the time required to add 100.000 elements as
-         * first element of the collection for both ArrayList and LinkedList,
-         * using the previous lists. In order to measure times, use as example
-         * TestPerformance.java.
-         */
-        /*
-         * 6) Measure the performance of reading 1000 times an element whose
-         * position is in the middle of the collection for both ArrayList and
-         * LinkedList, using the collections of point 5. In order to measure
-         * times, use as example PerfTest.java.
-         */
-        /*
-         * 7) Build a new Map that associates to each continent's name its
-         * population:
-         *
-         * Africa -> 1,110,635,000
-         *
-         * Americas -> 972,005,000
-         *
-         * Antarctica -> 0
-         *
-         * Asia -> 4,298,723,000
-         *
-         * Europe -> 742,452,000
-         *
-         * Oceania -> 38,304,000
-         */
-        /*
-         * 8) Compute the population of the world
-         */
+
+        Map<String, Long> map =new HashMap<>();
+        map.put("Africa", UseListsAndMaps.AFRICA_POP);
+        map.put("Americas", UseListsAndMaps.AMERICAS_POP);
+        map.put("Antartica", UseListsAndMaps.ANTARCTICA_POP);
+        map.put("Asia", UseListsAndMaps.ASIA_POP); 
+        map.put("Europe", UseListsAndMaps.EUROPE_POP);
+        map.put("Oceania", UseListsAndMaps.OCEANIA_POP); 
+
+        Long tot_population=0L;
+        for(Long population_amount : map.values()){
+            tot_population+=population_amount;
+        }
+
+        System.out.println("Popolazione totoale: "+tot_population);
+        
     }
 }
